f80c01c1ffd8e3ce53e64cac650f956ae4676e08
Kelvin Manalo <kimanalo04@gmail.com>
2021-02-22 01:59:13 +0800

    added comments to AED to array/objects

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -6,3 +6,18 @@ export default (state = [], action) => {
       return state;
   }
 };
+
+//add element to array
+// [...state, "new element"]
+
+//replaceing an element to array
+//state.map(el => el === "old element" ? "new element" : el)
+
+//removing an element to array
+//state.filter(el => el !== "element to delete")
+
+//add/update property to an object
+//{...state, name:"Allen"}
+
+//removing a property (use loadash)
+//_.omit(state,"property to remove")
